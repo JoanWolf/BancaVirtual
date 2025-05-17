8eafaa164a8e6a7ebc780fa12701adc8a3582070
Rojo-Web <santifa2004@gmail.com>
2025-05-17 15:21:29 -0500

    Funcionalidad de consulta para consulta de llave

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::resource('users',App\Http\Controllers\UserController::class);
 Route::resource('llaves',App\Http\Controllers\LlafeController::class);
 Route::resource('pqrs',App\Http\Controllers\PqrController::class);
 Route::resource('transacciones',App\Http\Controllers\transaccioneController::class);
+Route::get('/api/buscar', [App\Http\Controllers\LlafeController::class, 'buscarPorValor'])->name('llaves.buscar');
