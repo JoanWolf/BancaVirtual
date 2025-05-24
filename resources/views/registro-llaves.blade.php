1949cba92a9fac28094d2ef3f26f4a3ed225b7c6
JoanWolf <dasstian@hotmail.com>
2025-05-23 22:02:07 -0500

    adicion funcion de checkboxes y arreglo de padding

--- resources/views/registro-llaves.blade.php
+++ resources/views/registro-llaves.blade.php
@@ -13,26 +13,51 @@
             <div class="llave-item">
                 <span class="etiqueta">Llave del banco</span>
                 <span class="valor">@bandi12345</span>
-                <input type="checkbox">
+                <input type="checkbox" class="llave-checkbox">
             </div>
             <div class="llave-item">
                 <span class="etiqueta">C.C.</span>
                 <span class="valor">74185296</span>
-                <input type="checkbox">
+                <input type="checkbox" class="llave-checkbox">
             </div>
             <div class="llave-item">
                 <span class="etiqueta">Número de celular</span>
                 <span class="valor">3108647925</span>
-                <input type="checkbox">
+                <input type="checkbox" class="llave-checkbox">
             </div>
             <div class="llave-item">
                 <span class="etiqueta">Correo electrónico</span>
                 <span class="valor">Usuario@Dominio.Com</span>
-                <input type="checkbox">
+                <input type="checkbox" class="llave-checkbox">
             </div>
         </div>
 
-        <button class="btn-continuar">Continuar</button>
+        <button class="btn-continuar" id="btn-continuar" disabled>Continuar</button>
     </div>
 </div>
+
+<script>
+    const checkboxes = document.querySelectorAll('.llave-checkbox');
+    const btnContinuar = document.getElementById('btn-continuar');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', function () {
+            if (this.checked) {
+                checkboxes.forEach(cb => {
+                    if (cb !== this) cb.checked = false;
+                });
+                btnContinuar.disabled = false;
+                btnContinuar.classList.add('activo');
+            } else {
+                // Si ninguno queda seleccionado, deshabilitar el botón
+                const algunoMarcado = Array.from(checkboxes).some(cb => cb.checked);
+                if (!algunoMarcado) {
+                    btnContinuar.disabled = true;
+                    btnContinuar.classList.remove('activo');
+                }
+            }
+        });
+    });
+</script>
+
 
