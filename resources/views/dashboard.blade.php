a784e7edc5a9d3b8cc35937843637a4712f2684a
JoanWolf <dasstian@hotmail.com>
2025-05-28 19:27:11 -0500

    actualizacion dashboard admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,15 +2,40 @@
 
 <!-- Enlace al CSS específico del dashboard -->
 <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-@if (Auth::user()->Rol == "user" )
-<!-- Contenido específico del dashboard -->
-<div class="balance-card">
-    <p class="balance-title">Saldo Disponible</p>
-    <div class="balance-value">
-        <i class="fas fa-dollar-sign"></i>
-        <span>${{ number_format($saldo, 0, ',', '.') }}</span>
+@if (Auth::user()->Rol == 'user')
+    <!-- Contenido específico del dashboard -->
+    <div class="balance-card">
+        <p class="balance-title">Saldo Disponible</p>
+        <div class="balance-value">
+            <i class="fas fa-dollar-sign"></i>
+            <span>${{ number_format($saldo, 0, ',', '.') }}</span>
+        </div>
     </div>
-</div>
 @endif
-@if (Auth::user()->Rol == "admin" )
+@if (Auth::user()->Rol == 'admin')
+    <div class="container-dashadmin">
+        <div class="balance-card">
+            <p class="balance-title">Usuarios registrados</p>
+            <div class="balance-value">
+                <i class="fas fa-dollar-sign"></i>
+                <span>${{ number_format($saldo, 0, ',', '.') }}</span>
+            </div>
+        </div>
+
+        <div class="balance-card">
+            <p class="balance-title">Cantidad transacciones</p>
+            <div class="balance-value">
+                <i class="fas fa-dollar-sign"></i>
+                <span>${{ number_format($saldo, 0, ',', '.') }}</span>
+            </div>
+        </div>
+
+        <div class="balance-card">
+            <p class="balance-title">Cantidad PQRs</p>
+            <div class="balance-value">
+                <i class="fas fa-dollar-sign"></i>
+                <span>${{ number_format($saldo, 0, ',', '.') }}</span>
+            </div>
+        </div>
+    </div>
 @endif
